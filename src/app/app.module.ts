154d32270645cad30e5f8a7b3f17e963b642171c
jiangshanmeta <540118044@qq.com>
2023-01-02 11:20:48 +0800

    feat: bookList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
 
+import { NgxPaginationModule } from 'ngx-pagination'
 import { AppRoutingModule } from './app-routing.module'
+
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
 import { BookListComponent } from './book-list/book-list.component'
@@ -10,7 +12,7 @@ import { BookDetailComponent } from './book-detail/book-detail.component'
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, BookListComponent, BookDetailComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxPaginationModule],
     providers: [],
     bootstrap: [AppComponent]
 })
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Book, BookService } from '../book.service'
 
 @Component({
     selector: 'app-book-list',
     templateUrl: './book-list.component.html',
     styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {}
+export class BookListComponent implements OnInit {
+    books: Book[] = []
+    currentPage = 1
+    constructor(private bookService: BookService) {}
+
+    ngOnInit(): void {
+        this.bookService.getAllBooks().subscribe((books) => (this.books = [...books].reverse()))
+    }
+}
