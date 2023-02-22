88ff1d47212814d939309ec2950a66386b152d1c
jiangshanmeta <540118044@qq.com>
2023-02-22 13:14:54 +0800

    feat: cypress action

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 
 export default defineConfig( {
-  
+    projectId: "2a1ivb",
     e2e: {
         'baseUrl': 'http://localhost:4200',
         setupNodeEvents( on, config ) {
