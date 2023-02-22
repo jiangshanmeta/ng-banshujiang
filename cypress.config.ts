5a07d0e7ed4ead64a1a8ef16f7f497621ba0ac17
jiangshanmeta <540118044@qq.com>
2023-02-22 11:35:32 +0800

    feat: add e2e coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'cypress'
 
+
 export default defineConfig( {
   
     e2e: {
-        'baseUrl': 'http://localhost:4200'
+        'baseUrl': 'http://localhost:4200',
+        setupNodeEvents( on, config ) {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            require( '@cypress/code-coverage/task' )( on, config )
+
+            return config
+        },
     },
   
   
